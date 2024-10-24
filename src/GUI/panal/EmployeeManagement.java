7a4fac89e5d506ee426c87fc9a41d3a53857d661
Hashan2002 <hashantharaka890@gmail.com>
2024-10-24 22:41:31 +0530

    Hello
    H

--- src/GUI/panal/EmployeeManagement.java
+++ src/GUI/panal/EmployeeManagement.java
@@ -373,8 +373,8 @@ public class EmployeeManagement extends javax.swing.JPanel {
     }//GEN-LAST:event_jTextField3ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton3ActionPerformed
+        System.out.println("Hello");
+     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
         Employee_Address employee_Address = new Employee_Address((JFrame) SwingUtilities.getWindowAncestor(this), true);
