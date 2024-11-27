6c780bf58ef4e1f751feebaa5344d6cdbe77c97e
isuru20ip <isuru20.ip@gmail.com>
2024-11-27 16:41:57 +0530

    changed Bean

--- src/modal/beans/LogData.java
+++ src/modal/beans/LogData.java
@@ -1,32 +1,30 @@
 package modal.beans;
 
 import java.io.Serializable;
-import java.util.Vector;
+import java.util.LinkedList;
 
 public class LogData implements Serializable{
 
     /**
-     * @return the v
+     * @return the logList
      */
-    public Vector<String> getV() {
-        return v;
+    public LinkedList<String[]> getLogList() {
+        return logList;
     }
 
     /**
-     * @param v the v to set
+     * @param logList the logList to set
      */
-    public void setV(Vector<String> v) {
-        this.v = v;
+    public void setLogList(LinkedList<String[]> logList) {
+        this.logList = logList;
     }
 
     public LogData() {
     }
 
-    public LogData(Vector<String> v) {
-        this.v = v;
+    public LogData(LinkedList<String[]> logList) {
+        this.logList = logList;
     }
     
-    
-    
-    private Vector<String> v;
+    private LinkedList<String[]> logList;
 }
